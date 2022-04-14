d8d68afd9b5351d3ac2365b6fd61ff12243ec8ba
Stepan Lavrentev <lawrentievsv@gmail.com>
2022-04-14 13:00:36 +0300

    add description

--- derive_eth_publick_key_from_transaction.py
+++ derive_eth_publick_key_from_transaction.py
@@ -2,8 +2,17 @@ import web3
 from eth_account._utils.signing import extract_chain_id, to_standard_v
 from eth_account._utils.legacy_transactions import ALLOWED_TRANSACTION_KEYS, serializable_unsigned_transaction_from_dict
 
-w3 = web3.Web3(web3.WebsocketProvider('wss://wss.api.moonriver.moonbeam.network'))
-t = w3.eth.getTransaction(0xf01f8ea90a2e9e54ab61e938c0c5bc9bd296d7ba7730b0a18edab253a5b90927)
+'''
+For derive publick key from ETH transaction:
+1. Set blockchain node url
+2. Set transaction hash which will derive public key
+'''
+
+ws_url = 'wss://wss.api.moonriver.moonbeam.network'
+transaction_hash = 0xf01f8ea90a2e9e54ab61e938c0c5bc9bd296d7ba7730b0a18edab253a5b90927
+
+w3 = web3.Web3(web3.WebsocketProvider(ws_url))
+t = w3.eth.getTransaction(transaction_hash)
 
 s=w3.eth.account._keys.Signature(vrs=(to_standard_v(extract_chain_id(t.v)[1]), w3.toInt(t.r), w3.toInt(t.s)))
 
